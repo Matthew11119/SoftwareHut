7f896d42afb594e3a2bdd03ea9c123ed7c6b622a
Ming Lim <mjlim2@sheffield.ac.uk>
2019-03-05 11:27:24 +0000

    delete

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.session_store :active_record_store, key: (Rails.env.production? ? '_deployment-lab_session_id' : (Rails.env.demo? ? '_deployment-lab_demo_session_id' : '_deployment-lab_dev_session_id')),
-                                                             secure: (Rails.env.demo? || Rails.env.production?),
-                                                             httponly: true
