2560411fba99cf1f5b584b4b64dc21393241755c
nelson <mjlim2@sheffield.ac.uk>
2019-03-18 10:37:25 +0000

    test cases specify only

---
+++ spec/features/managing_users.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe 'Managing users' do
+    
+    specify  'I can add an admin' do 
+    end
+
+    specify 'I can delete an admin ' do
+    end
+
+    specify 'I can add a module lead' do
+    end
+
+    specify 'I can delete a module lead' do
+    end
+
+    specify 'I can import students from a csv' do
+    end
+
+    specify 'I can add new student from a form' do
+    end
+
+    specify 'I can delete a student' do
+    end
+
+end
