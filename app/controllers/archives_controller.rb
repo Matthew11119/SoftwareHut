4d940e3ae1fbc6c0efa48f4f857baf07e476798a
Lucas Maybury <lucasmaybury@gmail.com>
2019-03-19 12:39:06 +0000

    Controller and Views for Archvive and Templates Various comment updates

---
+++ app/controllers/archives_controller.rb
@@ -0,0 +1,38 @@
+class ArchivesController < ApplicationController
+  before_action :set_archive, only: [:show, :edit, :update, :destroy]
+
+  # GET /archives
+  def index
+    @archives = Exam.archive
+  end
+
+  # GET /archives/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /archives/1
+  def update
+    if @archive.update(archive_params)
+      redirect_to @archive, notice: 'Archive was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /archives/1
+  def destroy
+    @archive.destroy
+    redirect_to archives_url, notice: 'Archive was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_archive
+      @archive = Archive.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def archive_params
+      params.fetch(:archive, {})
+    end
+end
--- spec/models/exam_spec.rb
+++ spec/models/exam_spec.rb
@@ -3,13 +3,13 @@
 # Table name: exams
 #
 #  id          :bigint(8)        not null, primary key
+#  date        :date
 #  exam_code   :string
-#  exam_date   :date
-#  exam_name   :string
 #  module_code :string
+#  name        :string
+#  status      :integer
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
-#  exam_id     :integer
 #
 
 require 'rails_helper'
