f89ede3769c45362d8b4d70fa36f4ee9513102f1
nelson <mjlim2@sheffield.ac.uk>
2019-03-20 13:53:48 +0000

    fixed query for station and able to view stations for selected exam

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -48,7 +48,8 @@ class StationsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_station
-      @station = Station.find(params[:id])
+      # @station = Station.find(params[:id])
+      @station = Station.where(:exam_code=>params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_130724) do
+ActiveRecord::Schema.define(version: 2019_03_20_134836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,9 +85,9 @@ ActiveRecord::Schema.define(version: 2019_03_20_130724) do
   create_table "stations", force: :cascade do |t|
     t.string "station_name"
     t.integer "pass_mark"
-    t.integer "exam_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "exam_code"
   end
 
   create_table "students", force: :cascade do |t|
