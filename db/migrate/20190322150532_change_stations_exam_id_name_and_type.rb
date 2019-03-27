a197b770a494e059306991589fe3f151061c2793
Ming Lim <mjlim2@sheffield.ac.uk>
2019-03-27 14:17:58 +0000

    Delete 20190322150532_change_stations_exam_id_name_and_type.rb

--- db/migrate/20190322150532_change_stations_exam_id_name_and_type.rb
+++
@@ -1,6 +0,0 @@
-class ChangeStationsExamIdNameAndType < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :stations, :exam_id,   :integer
-    add_column    :stations, :exam_code, :string
-  end
-end
