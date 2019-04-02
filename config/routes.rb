499fe329178e58f180d8ef8b81c3e680d671cd25
nelson <mjlim2@sheffield.ac.uk>
2019-04-02 13:21:59 +0100

    edited resources to only

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :uni_modules
   resources :criteria_results
   resources :station_results
-  resources :stations do
+  resources :stations, only: [:show] do
     member do
       get 'detail'
       get 'detail_form'
