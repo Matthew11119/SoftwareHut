f8db1750e85eb69d2e4bde21004199b5fc0d87e7
Lucas Maybury <lucasmaybury@gmail.com>
2019-04-12 20:18:08 +0100

    Migrate after revert

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_104330) do
+ActiveRecord::Schema.define(version: 2019_04_10_105115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_104330) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "exam_id"
+    t.integer "template_id"
   end
 
   create_table "students", id: false, force: :cascade do |t|
@@ -108,6 +109,12 @@ ActiveRecord::Schema.define(version: 2019_04_10_104330) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.index ["regno"], name: "index_students_on_regno"
+  end
+
+  create_table "templates", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "uni_modules", primary_key: "module_code", id: :string, force: :cascade do |t|
