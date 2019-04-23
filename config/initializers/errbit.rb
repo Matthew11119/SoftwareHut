cfc82d71edd2e646b2b64f856b9a18eabdf2b3de
Lucas <lucasmaybury@gmail.com>
2019-04-23 15:38:43 +0100

    configured errbit

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,5 +1,17 @@
+# Require the airbrake gem in your App.
+# ---------------------------------------------
+#
+# Rails 3 - In your Gemfile
+# gem 'airbrake'
+#
+# Rails 2 - In environment.rb
+# config.gem 'airbrake'
+#
+# Then add the following to config/initializers/errbit.rb
+# -------------------------------------------------------
+
 Airbrake.configure do |config|
-  config.api_key = 'REPLACE_ME'
+  config.api_key = '505fa663fe5e6dfa6529414377bba08f'
   config.host    = 'errbit.hut.shefcompsci.org.uk'
   config.port    = 443
   config.secure  = config.port == 443
