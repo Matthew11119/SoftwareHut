e9416849597a92e247d7b30996ad8d524863e48c
nelson <mjlim2@sheffield.ac.uk>
2019-04-25 12:26:18 +0100

    Module Lead Ready Screen in StationResults

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,7 +7,8 @@ class Ability
     if user.user_type == "Admin"
       can :manage, :all
     elsif user.user_type == "Module Lead"
-      can [:read, :edit, :update, :ready_screen], [StationResult, CriteriaResult]
+      can [:read, :edit, :update], [CriteriaResult]
+      can [:ready_screen, :show], [StationResult]
       can [:index, :show], [Exam]
     elsif user.user_type == "Moderator"
       can [:read], [StationResult, CriteriaResult]
