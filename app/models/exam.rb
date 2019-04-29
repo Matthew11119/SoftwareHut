21e47ac59cf0f4395091741d88ccdb5de4928365
nelson <mjlim2@sheffield.ac.uk>
2019-04-29 20:00:53 +0100

    remove commented functions

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -11,11 +11,9 @@
 #  updated_at  :datetime         not null
 #
 
-class Exam < ApplicationRecord
-  #has_and_belongs_to_many :students
+class Exam < ApplicationRecord  
   has_many :exams_students
   has_many :students, :through => :exams_students
-
   has_many :stations, inverse_of: :exam
 
   scope :undeployed, -> { where(status: 0).order(:date) }
@@ -24,16 +22,14 @@ class Exam < ApplicationRecord
   scope :archive, -> { where(status: 3).order(:date) }
   scope :templates, -> { where(status: 4).order(:date) }
 
-
+  # CSV import
   def student_import(file)
     CSV.foreach(file.path, headers: true) do |row|
       if !Student.exists?(:username =>row['username'])
         self.students << Student.new(row.to_hash)
-
       elsif (!self.students.exists?(:username =>row['username']))
         self.students << Student.find(row['username'])
       end
     end
   end
-
 end
